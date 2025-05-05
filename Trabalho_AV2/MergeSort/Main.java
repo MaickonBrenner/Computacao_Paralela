ecc70a2ad9c61a72043b7a15b1803f89e64f3eb1
Maickon_Brenner <maickonbrenner@gmail.com>
2025-05-05 18:47:05 -0300

    Alterações MergeSort

--- Trabalho_AV2/MergeSort/Main.java
+++ Trabalho_AV2/MergeSort/Main.java
@@ -1,10 +1,79 @@
 package Trabalho_AV2.MergeSort;
 
-public class Main {
-    private static final int[] Threads = {1, 2, 4, 8, 16};
-    private static final int[] Tamanho_Problema = {1000, 5000, 10000, 20000, 50000};
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.Arrays;
 
+public class Main {
+    private static final int[] Threads = {2, 4, 8, 10, 16};
+    private static final int[] Tamanho_Problema = {10, 50, 80, 100, 200};
     public static void main(String[] args) {
+        MergeSortSerial mergeSerial = new MergeSortSerial();
+        MergeSortParalelo mergeParalelo = new MergeSortParalelo();
+
+        try (FileWriter arquivo = new FileWriter("merge_resultados.csv")) {
+            arquivo.append("Tipo,Tamanho_Array,Threads,Tempo(ms),Tempo(ns)\n");
+
+            // Serial
+            int[] arraySerial = gerarArray(1000);
+            long serialTempoInicial = System.nanoTime();
+            mergeSerial.mergeSort(arraySerial, arraySerial.length);
+            long serialTempoFinal = System.nanoTime();
+            long tempoSerial = calculaTempo(serialTempoInicial, serialTempoFinal);
+
+            arquivo.append(String.format("Serial,%d,%d,%d,%d,\n", arraySerial.length, 1, tempoSerial/1000000, tempoSerial));
+
+            // Paralelo 
+            for (int i = 0; i < 5; i++) {
+                int num_sorteio = sortearTamanho(Tamanho_Problema);
+                int tamanho = Tamanho_Problema[num_sorteio];
+                int[] arrayParalelo = gerarArray(tamanho);
+                System.out.println("Array: " + Arrays.toString(arrayParalelo));
+
+                int numThreads = Threads[num_sorteio];
+
+                ExecutorService executor = Executors.newFixedThreadPool(numThreads);
+
+                long paraleloTempoInicial = System.nanoTime();
+                mergeParalelo.mergeSort(arrayParalelo, executor);
+                long paraleloTempoFinal = System.nanoTime();
+                long tempoParalelo = calculaTempo(paraleloTempoInicial, paraleloTempoFinal);
+
+                executor.shutdown();
         
+                System.out.println("Tamanho: " + tamanho + " | Número de Threads" + numThreads + " para o problema.");
+
+                arquivo.append(String.format("Paralelo,%d,%d,%d,%d,\n", tamanho, numThreads, tempoParalelo/1000000, tempoParalelo));
+                
+            }
+
+            System.out.println("Arquivo CSV gerado na raiz Trabalho_AV2!");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }     
+    }
+
+    private static int[] gerarArray(int tamanho) {
+        Random random = new Random();
+        int[] array = new int[20];
+        for (int i = 0; i < 20; i++) {
+            array[i] = random.nextInt(tamanho);
+        }
+        return array;
+    }
+
+    private static long calculaTempo(long serialTempoInicial, long serialTempoFinal) {
+        long valor = serialTempoFinal - serialTempoInicial;
+        return valor;
+    }
+
+    private static int sortearTamanho(int[] valores) {
+        Random random = new Random();
+        int tamanho_sorteado = random.nextInt(valores.length);
+        return tamanho_sorteado;
     }
 }
--- Trabalho_AV2/MergeSort/MergeSortParalelo.java
+++ Trabalho_AV2/MergeSort/MergeSortParalelo.java
@@ -2,18 +2,11 @@ package Trabalho_AV2.MergeSort;
 
 import java.util.Arrays;
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
 public class MergeSortParalelo {
-    public void iniciarMergeSort(int[] a) {
-		int numThreads = 10;
-		ExecutorService executor = Executors.newFixedThreadPool(numThreads);
-        mergeSort(a, executor);
-        executor.shutdown();
-	}
 	
-	private void mergeSort(int[] a, ExecutorService executor) {
+	public void mergeSort(int[] a, ExecutorService executor) {
 		if (a.length < 2) {
 			return;
 		}
@@ -22,10 +15,11 @@ public class MergeSortParalelo {
 		int[] left = Arrays.copyOfRange(a, 0, mid);
 		int[] right = Arrays.copyOfRange(a, mid, a.length);
 		
-		Future<?> leftFuture = executor.submit(() -> mergeSort(left, executor));
-		Future<?> rightFuture = executor.submit(() -> mergeSort(right, executor));
-		
 		try {
+			Future<?> leftFuture = executor.submit(() -> mergeSort(left, executor));
+			Future<?> rightFuture = executor.submit(() -> mergeSort(right, executor));
+		
+
 			leftFuture.get();
 			rightFuture.get();
 		} catch (Exception e) {
