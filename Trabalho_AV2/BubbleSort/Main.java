e6edb563d0312a0585d3da7986160e053c18057e
Luccs3003 <lucascardoso4283@gmail.com>
2025-05-09 17:56:17 -0300

    update main's

--- Trabalho_AV2/BubbleSort/Main.java
+++ Trabalho_AV2/BubbleSort/Main.java
@@ -1,24 +1,26 @@
-package Trabalho_AV2.QuickSort;
+package Trabalho_AV2.BubbleSort;
 
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Main {
     private static final int[] Threads = {1, 2, 4, 8, 16};
     private static final int[] Tamanho_Problema = {10000, 50000, 100000, 200000, 500000};
 
     public static void main(String[] args) {
-        QuickSortSerial quickSerial = new QuickSortSerial();
-        QuickSortParalelo quickParalelo = new QuickSortParalelo();
+        BubbleSortSerial bubbleSerial = new BubbleSortSerial();
+        BubbleSortParalelo bubbleParalelo = new BubbleSortParalelo();
 
-        try (FileWriter arquivo = new FileWriter("Trabalho_AV2/Resultados/QuickSort_resultados.csv")) {
+        try (FileWriter arquivo = new FileWriter("Trabalho_AV2/Resultados/BubbleSort_resultados.csv")) {
             arquivo.append("Tipo,Tamanho_Array,Threads,Tempo(ms),Tempo(ns)\n");
 
             // Serial
             int[] arraySerial = gerarArray(1000);
             long tempoInicialSerial = System.nanoTime();
-            QuickSortSerial.sort(arraySerial, 0, arraySerial.length - 1);
+            bubbleSerial.sort(arraySerial);
             long tempoFinalSerial = System.nanoTime();
             long tempoSerial = calculaTempo(tempoInicialSerial, tempoFinalSerial);
             arquivo.append(String.format("Serial,%d,%d,%d,%d,\n", arraySerial.length, 1, tempoSerial / 1_000_000, tempoSerial));
@@ -28,16 +30,18 @@ public class Main {
                 int tamanho = Tamanho_Problema[i];
                 int numThreads = Threads[i];
                 int[] arrayParalelo = gerarArray(tamanho);
+                ExecutorService executor = Executors.newFixedThreadPool(numThreads);
 
                 long tempoInicialParalelo = System.nanoTime();
-                QuickSortParalelo.parallelQuickSort(arrayParalelo, numThreads);
+                bubbleParalelo.parallelBubbleSort(arrayParalelo, numThreads);
                 long tempoFinalParalelo = System.nanoTime();
+                executor.shutdown();
 
                 long tempoParalelo = calculaTempo(tempoInicialParalelo, tempoFinalParalelo);
                 arquivo.append(String.format("Paralelo,%d,%d,%d,%d,\n", tamanho, numThreads, tempoParalelo / 1_000_000, tempoParalelo));
             }
 
-            System.out.println("Arquivo QuickSort_resultados.csv gerado na pasta Resultados!");
+            System.out.println("Arquivo BubbleSort_resultados.csv gerado na pasta Resultados!");
 
         } catch (IOException e) {
             e.printStackTrace();
