0e4909edbafff2cc3e9773a714dd7ffc5889ab07
Maickon_Brenner <maickonbrenner@gmail.com>
2025-05-09 22:09:22 -0300

    Ajustes nos arrays

--- Trabalho_AV2/InsertionSort/Main.java
+++ Trabalho_AV2/InsertionSort/Main.java
@@ -8,7 +8,7 @@ import java.util.concurrent.Executors;
 
 class Main {
     private static final int[] Threads = {1, 2, 4, 8, 16};
-    private static final int[] Tamanho_Problema = {10000, 50000, 100000, 200000, 500000};
+    private static final int[] Tamanho_Problema = {1000, 5000, 10000, 20000, 50000};
     public static void main(String[] args) {
         InsertionSortSerial insertionSerial = new InsertionSortSerial();
         InsertionSortParalelo insertionParalelo = new InsertionSortParalelo();
